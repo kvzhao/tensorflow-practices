34e630907f2209eb6d6c9e4af20cf48c0ef2c462
kvzhao <kelispinor@gmail.com>
2017-09-22 16:45:18 +0800

    FLAGS members should be used first

--- tf_flags.py
+++ tf_flags.py
@@ -6,10 +6,19 @@ tf.app.flags.DEFINE_string('name', 'model_name', 'String example')
 
 FLAGS = tf.app.flags.FLAGS
 
+# Call first
+num = FLAGS.number
+name = FLAGS.name
+
 config = dict(FLAGS.__flags.items())
 print (config)
 
 with open('config.json', 'w') as file:
     json.dump(config, file)
 
-# SHIT, it's empty! why?
+with open('config.json') as data_file:    
+    saved = json.load(data_file)
+print (type(saved))
+
+for k, v in saved.items():
+    print (k, v)
